336044a32903e7e28b0fc7d4cd7879386d626bd9
박준혁 <junna961@icloud.com>
2026-06-11 00:34:58 +0900

    Fix iOS calendar widget plugin registration

---
+++ ios/App/App/MainViewController.swift
@@ -0,0 +1,9 @@
+import Capacitor
+import UIKit
+
+class MainViewController: CAPBridgeViewController {
+    override func capacitorDidLoad() {
+        super.capacitorDidLoad()
+        bridge?.registerPluginInstance(CalendarWidgetsPlugin())
+    }
+}
