da3c1136fad417971f8a8df86d5b46b5188143b4
박준혁 <junna961@icloud.com>
2026-06-24 14:21:15 +0900

    Fix character effect positioning

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 
-const SITE_VERSION = '20260624-01';
+const SITE_VERSION = '20260624-02';
 
 const STATIC_CACHE = `mallin-static-${SITE_VERSION}`;
 const HTML_CACHE = `mallin-html-${SITE_VERSION}`;
