956a179cabd33436a70d59fc95b1c44687ae37bf
박준혁 <junna961@icloud.com>
2026-07-25 00:35:43 +0900

    캘린더 저장 중 문구 한 줄 표시

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 
-const SITE_VERSION = '20260724-05';
+const SITE_VERSION = '20260725-01';
 
 const STATIC_CACHE = `mallin-static-${SITE_VERSION}`;
 const HTML_CACHE = `mallin-html-${SITE_VERSION}`;
