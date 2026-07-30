78565f545e4d51dc4a4bf9910fd42b2644a78427
박준혁 <junna961@icloud.com>
2026-07-30 16:52:00 +0900

    앱 1.0.14 릴리스 버전 갱신

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 
-const SITE_VERSION = '20260730-02';
+const SITE_VERSION = '20260730-03';
 
 const STATIC_CACHE = `mallin-static-${SITE_VERSION}`;
 const HTML_CACHE = `mallin-html-${SITE_VERSION}`;
